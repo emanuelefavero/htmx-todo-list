89d7b4b4f94aa4da05277df5c45ceac9e1484d09
emanuelefavero <emafaux89@gmail.com>
2024-04-26 23:16:39 +0200

    add - clear input after form post request

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,27 @@ const apiRouter = Router()
 // Calculate directory path
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// API routes
-// * GET /api/todos
-apiRouter.get('/api/todos', (req, res) => {
-  const todosHTML = todos
+function generateTodosHTML(todos) {
+  return todos
     .map((todo) => {
       return `<li class="${todo.completed && 'line-through'}">${todo.text}</li>`
     })
     .join('')
+}
 
-  res.send(todosHTML)
+// API routes
+// * GET /api/todos
+apiRouter.get('/api/todos', (req, res) => {
+  res.send(generateTodosHTML(todos))
+})
+
+// * POST /api/todos - add a new todo
+apiRouter.post('/api/todos', (req, res) => {
+  const { text } = req.body
+  const newTodo = { id: todos.length + 1, text, completed: false }
+  todos.push(newTodo)
+
+  res.send(generateTodosHTML(todos))
 })
 
 export default apiRouter
