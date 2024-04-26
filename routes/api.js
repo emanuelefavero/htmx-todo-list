76aab571689cc12574754d208cef50ce85be8fda
emanuelefavero <emafaux89@gmail.com>
2024-04-26 23:33:35 +0200

    add - toggle todo

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 function generateTodosHTML(todos) {
   return todos
     .map((todo) => {
-      return `<li class="${todo.completed && 'line-through'}">${todo.text}</li>`
+      return `<li hx-post="/api/toggle/${
+        todo.id
+      }" hx-trigger="click" hx-target="#todos" class="${
+        todo.completed ? 'line-through' : ''
+      }">${todo.text}</li>`
     })
     .join('')
 }
@@ -27,6 +31,17 @@ apiRouter.post('/api/todos', (req, res) => {
   const { text } = req.body
   const newTodo = { id: todos.length + 1, text, completed: false }
   todos.push(newTodo)
+
+  res.send(generateTodosHTML(todos))
+})
+
+// * POST /api/toggle/:id - toggle the completion status of a todo
+apiRouter.post('/api/toggle/:id', (req, res) => {
+  const { id } = req.params
+  const todo = todos.find((t) => t.id == id)
+  if (todo) {
+    todo.completed = !todo.completed
+  }
 
   res.send(generateTodosHTML(todos))
 })
