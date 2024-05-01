8a8c94fd532f3df4f6f9e835117c9fe04344fad6
emanuelefavero <emafaux89@gmail.com>
2024-05-01 19:27:05 +0200

    use __dirname import in pagesRouter

--- routes/pagesRouter.js
+++ routes/pagesRouter.js
@@ -1,19 +1,12 @@
 import { Router } from 'express'
 import path from 'path'
-import { fileURLToPath } from 'url'
+import __dirname from '../utilities/__dirname.js'
 
 const pagesRouter = Router()
 
-// Calculate directory path
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-// Routes for serving HTML pages
-pagesRouter.get('/home', (req, res) => {
+// * GET / - Home page
+pagesRouter.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
 })
-
-// pagesRouter.get('/posts', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../pages/posts.html'))
-// })
 
 export default pagesRouter
--- utilities/__dirname.js
+++ utilities/__dirname.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+// Calculate directory path
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default __dirname
