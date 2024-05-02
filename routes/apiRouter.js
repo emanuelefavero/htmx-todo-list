62aaed50753af4e69aeb50edf8d2d5bb11ea4852
emanuelefavero <emafaux89@gmail.com>
2024-05-02 18:46:28 +0200

    add - new todo, toggle todo database implementation

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { v4 as uuidv4 } from 'uuid'
-import { todos } from '../data/todos.js'
+// import { todos } from '../data/todos.js'
 import pool from '../config/database.js'
 import generateTodosHTML from '../utilities/generateTodosHTML.js'
 
@@ -9,8 +9,13 @@ const apiRouter = Router()
 // * GET /api/todos - get all todos
 apiRouter.get('/api/todos', async (req, res) => {
   try {
+    // * Fetch all todos from the PostgreSQL database with `pg` pool
     const { rows } = await pool.query('SELECT * FROM todos')
+
+    // Send todos as HTML
     res.send(generateTodosHTML(rows)) // ? rows is the array of todos
+
+    // Error handling
   } catch (error) {
     console.error(error)
     res
@@ -20,34 +25,65 @@ apiRouter.get('/api/todos', async (req, res) => {
 })
 
 // * POST /api/todos - add a new todo
-apiRouter.post('/api/todos', (req, res) => {
+apiRouter.post('/api/todos', async (req, res) => {
   const { text } = req.body
-  const newTodo = { id: uuidv4(), text, completed: false }
-  todos.unshift(newTodo) // add new todo at the beginning
 
-  res.send(generateTodosHTML(todos))
+  // * Insert a new todo into the PostgreSQL database with `pg` pool
+  try {
+    await pool.query(
+      'INSERT INTO todos (id, text, completed) VALUES ($1, $2, $3)',
+      [uuidv4(), text, false]
+    )
+
+    // Fetch all todos from the database
+    const { rows } = await pool.query('SELECT * FROM todos')
+
+    // Send todos as HTML
+    res.send(generateTodosHTML(rows))
+
+    // Error handling
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .send('An error occurred while adding a new todo to the database')
+  }
 })
 
 // * POST /api/todos/toggle/:id - toggle the completion status of a todo
-apiRouter.post('/api/todos/toggle/:id', (req, res) => {
+apiRouter.post('/api/todos/toggle/:id', async (req, res) => {
   const { id } = req.params
-  const todo = todos.find((t) => t.id === id)
-  if (todo) {
-    todo.completed = !todo.completed
 
-    // Put todo at the top if it's toggled to not completed
-    if (!todo.completed) {
-      const index = todos.findIndex((t) => t.id === id)
-      todos.splice(index, 1)
-      todos.unshift(todo)
+  try {
+    // Toggle the completion status of the todo
+    const toggleResponse = await pool.query(
+      `UPDATE todos 
+      SET completed = NOT completed 
+      WHERE id = $1 
+      RETURNING *;`,
+      [id]
+    )
+    if (toggleResponse.rows.length === 0) {
+      return res.status(404).send('<p>Todo not found</p>')
     }
-  }
 
-  res.send(generateTodosHTML(todos))
+    // Put not completed todos at the top of the list and send as HTML
+    const { rows } = await pool.query(
+      `SELECT * FROM todos 
+      ORDER BY completed ASC, (id = $1) DESC;`,
+      [id]
+    )
+    res.send(generateTodosHTML(rows))
+
+    // Error handling
+  } catch (error) {
+    console.error('Error toggling todo completion', error)
+    res.status(500).send('<p>Internal server error</p>')
+  }
 })
 
 // * POST /api/delete/:id - delete a todo
-apiRouter.post('/api/todos/delete/:id', (req, res) => {
+apiRouter.post('/api/todos/delete/:id', async (req, res) => {
   const { id } = req.params
   const index = todos.findIndex((t) => t.id === id)
   if (index !== -1) todos.splice(index, 1)
@@ -56,7 +92,7 @@ apiRouter.post('/api/todos/delete/:id', (req, res) => {
 })
 
 // * POST /api/delete-completed - delete all completed todos
-apiRouter.post('/api/todos/delete-completed', (req, res) => {
+apiRouter.post('/api/todos/delete-completed', async (req, res) => {
   for (let i = todos.length - 1; i >= 0; i--) {
     if (todos[i].completed) {
       todos.splice(i, 1)
